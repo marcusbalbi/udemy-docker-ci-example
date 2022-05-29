18fb9c374643e0a1a117979cc0946390aef7d664
Marcus Balbi <balbimarcus@gmail.com>
2022-05-29 11:10:06 -0300

    working with tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('3renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
